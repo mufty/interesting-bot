7bca1a85c5c6899b46c8f373910c8beef9c2504e
Michal Gubriansky <gubriansky@sernato.sk>
2017-12-19 21:36:12 +0100

    localization added

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
+const i18next = require('i18next');
+
+const _en = require('./src/resources/locale/en.json');
 
 const App = require('./src/main/app');
 
-App.run();
+i18next.init({
+  lng: 'en',
+  debug: true,
+  resources: {
+    en: {
+      translation: _en
+    }
+  }
+}, function() {
+  App.run();
+});
 
 exports.botStatus = functions.https.onRequest((request, response) => {
   var _adapter = App.getAdapter('discordAdapter1');
